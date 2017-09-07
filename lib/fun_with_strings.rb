c3a2ede38d450bf4ce9f67b47cf1d40cdaa3ebf8
Samuel Murray <smurra28@uncc.edu>
2017-09-07 11:58:06 -0400

    Part1: A & B

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,9 +1,18 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    self.downcase.gsub(/\W+/, "") == self.downcase.gsub(/\W+/, "").reverse
   end
   def count_words
-    # your code here
+    words = Hash.new
+    phrase = self.downcase.gsub(/(^\s+|\W$)/, "").scan(/\w+/)
+    phrase.each do |word|
+      if words.key? word
+        words[word] += 1
+      else
+        words[word] = 1
+      end
+    end
+  return words
   end
   def anagram_groups
     # your code here
--- spec/fun_with_strings_spec.rb
+++ spec/fun_with_strings_spec.rb
@@ -15,7 +15,7 @@ describe 'palindrome detection' do
   end    
 end
 
-describe 'word count', :pending => true do
+describe 'word count'  do
   it 'should return a hash [5 points]' do
     expect('now is the time'.count_words).to be_a_kind_of(Hash)
   end
