4a83f6b2756e85f76a3660f361fb300c0fdf30d0
Patherceve <15074780+Patherceve@users.noreply.github.com>
2017-09-17 11:07:24 -0400

    first scratch part 5
    Your thoughs? Also I didn't understood how to test it... The irb gave me a hard time, but I could end up with the expected result:
    
    2.4.0 :001 > class CartesianProduct
    2.4.0 :002?>     include Enumerable
    2.4.0 :003?>     # YOUR CODE HERE
    2.4.0 :004 >       attr_accessor :sequences0
    2.4.0 :005?>     attr_accessor :sequences1
    2.4.0 :006?>     def initialize(sequences0, sequences1)
    2.4.0 :007?>         @sequences0 = sequences0
    2.4.0 :008?>         @sequences1 = sequences1
    2.4.0 :009?>       end
    2.4.0 :010?>     def cProd
    2.4.0 :011?>         hold = Array.new
    2.4.0 :012?>         
    2.4.0 :013 >           for c0 in @sequences0 
    2.4.0 :014?>             for c1 in @sequences1
    2.4.0 :015?>                 hold.push([c0, c1])
    2.4.0 :016?>               end
    2.4.0 :017?>           end
    2.4.0 :018?>         
    2.4.0 :019 >           return hold
    2.4.0 :020?>       end
    2.4.0 :021?>   end
     => :cProd 
    2.4.0 :022 > test = CartesianProduct.new(['a', 'b', 'c'], [4, 5])                                                                
     => #<CartesianProduct:0x00000001f6f828 @sequences0=["a", "b", "c"], @sequences1=[4, 5]> 
    2.4.0 :023 > test.cProd
     => [["a", 4], ["a", 5], ["b", 4], ["b", 5], ["c", 4], ["c", 5]]

--- lib/cartesian_product.rb
+++ lib/cartesian_product.rb
@@ -1,4 +1,19 @@
 class CartesianProduct
   include Enumerable
   # YOUR CODE HERE
+  attr_accessor :sequences0
+  attr_accessor :sequences1
+  def initialize(sequences0, sequences1)
+    @sequences0 = sequences0
+    @sequences1 = sequences1
+  end
+  def cProd
+    hold = Array.new
+    for c0 in @sequences0 
+      for c1 in @sequences1
+        hold.push([c0, c1])
+      end
+    end
+    return hold
+  end
 end
